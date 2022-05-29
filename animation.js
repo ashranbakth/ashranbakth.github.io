c0cb348c7495e50b5e7bda55666212d1e4719e8f
Ashran Bakth <ashranbakth@Ashrans-MBP.attlocal.net>
2022-05-28 21:19:39 -0400

    made a small text change

--- animation.js
+++ animation.js
@@ -3,7 +3,7 @@ var txt = `
 A software engineer that has experience working in the automotive and 
 financial mortgage industry building web applications. Have experience using
 frameworks such as Angular for the front-end and C# .NET or Java Spring
-for back-end. Small experience in developing CI/CD pipeline using CircleCI.
+for the back-end. Small experience in developing CI/CD pipeline using CircleCI.
 `;
 
 var speed = 50; 
